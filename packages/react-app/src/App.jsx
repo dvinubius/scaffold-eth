98995e8c504998f4c3013ff7fe850b85d9de76c4
Dinu Barbu <dinu@MacBook-Pro.local>
2021-12-06 20:26:01 +0200

    back to localhost in config and App.jsx

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -54,7 +54,7 @@ import {
 */
 
 /// 📡 What chain are your contracts deployed to?
-const targetNetwork = NETWORKS.rinkeby; // <------- select your target frontend network (localhost, rinkeby, xdai, mainnet)
+const targetNetwork = NETWORKS.localhost; // <------- select your target frontend network (localhost, rinkeby, xdai, mainnet)
 
 // 😬 Sorry for all the console logging
 const DEBUG = true;
